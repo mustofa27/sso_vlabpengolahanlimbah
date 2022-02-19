5a29111c5d7ef8124cdcb7694ec0a472a0a7e09b
Ahmad Mustofa <mustofaahmad327@gmail.com>
2022-02-19 21:27:39 +0700

    bug logout

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -11,27 +11,31 @@ class MainController extends Controller
     //
     public function index()
     {
-        try {
-            $oidc = new OpenIDConnectClient(
-                        'https://my.its.ac.id', // authorization_endpoint
-                        '9B0B5CC9-1166-4ABA-AC51-F4657FC00710', // Client ID
-                        '1aiqu0dn1z0g8s04c4c488s4' // Client Secret
-                    );
-        
-            $oidc->setRedirectURL('http://riset.its.ac.id/praktikum/vlab-pengolahanlimbah'); // must be the same as you registered
-            $oidc->addScope('email group phone profile role openid'); //must be the same as you registered
+        if (!isset($_SESSION['id_token'])) {
+            try {
+                $oidc = new OpenIDConnectClient(
+                            'https://my.its.ac.id', // authorization_endpoint
+                            '9B0B5CC9-1166-4ABA-AC51-F4657FC00710', // Client ID
+                            '1aiqu0dn1z0g8s04c4c488s4' // Client Secret
+                        );
             
-            // remove this if in production mode
-            $oidc->setVerifyHost(false);
-            $oidc->setVerifyPeer(false);
-        
-            $oidc->authenticate(); //call the main function of myITS SSO login
-        
-            $_SESSION['id_token'] = $oidc->getIdToken(); // must be save for check session dan logout proccess
-            $user = $oidc->requestUserInfo(); // this will return user information from myITS SSO database
+                $oidc->setRedirectURL('http://riset.its.ac.id/praktikum/vlab-pengolahanlimbah'); // must be the same as you registered
+                $oidc->addScope('email group phone profile role openid'); //must be the same as you registered
+                
+                // remove this if in production mode
+                $oidc->setVerifyHost(false);
+                $oidc->setVerifyPeer(false);
+            
+                $oidc->authenticate(); //call the main function of myITS SSO login
+            
+                $_SESSION['id_token'] = $oidc->getIdToken(); // must be save for check session dan logout proccess
+                $user = $oidc->requestUserInfo(); // this will return user information from myITS SSO database
+                return view('welcome');
+            } catch (OpenIDConnectClientException $e) {
+                echo $e->getMessage();
+            }
+        } else{
             return view('welcome');
-        } catch (OpenIDConnectClientException $e) {
-            echo $e->getMessage();
         }
     }
     public function logout()
