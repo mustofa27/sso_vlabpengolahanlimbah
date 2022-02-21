85a7620709b34d7ecc58ac1e519629a7374ad2fc
Ahmad Mustofa <mustofaahmad327@gmail.com>
2022-02-22 04:52:15 +0700

    bug modul

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -138,32 +138,33 @@ class MainController extends Controller
 
     public function anaerobik()
     {
-        if (!Session::has('id_token')) {
-            try {
-                $oidc = new OpenIDConnectClient(
-                            'https://my.its.ac.id', // authorization_endpoint
-                            '9B0B5CC9-1166-4ABA-AC51-F4657FC00710', // Client ID
-                            '1aiqu0dn1z0g8s04c4c488s4' // Client Secret
-                        );
+        echo "menuju anaerobik";
+        // if (!Session::has('id_token')) {
+        //     try {
+        //         $oidc = new OpenIDConnectClient(
+        //                     'https://my.its.ac.id', // authorization_endpoint
+        //                     '9B0B5CC9-1166-4ABA-AC51-F4657FC00710', // Client ID
+        //                     '1aiqu0dn1z0g8s04c4c488s4' // Client Secret
+        //                 );
             
-                $oidc->setRedirectURL('http://riset.its.ac.id/praktikum/vlab-pengolahanlimbah'); // must be the same as you registered
-                $oidc->addScope('email group phone profile role openid'); //must be the same as you registered
+        //         $oidc->setRedirectURL('http://riset.its.ac.id/praktikum/vlab-pengolahanlimbah'); // must be the same as you registered
+        //         $oidc->addScope('email group phone profile role openid'); //must be the same as you registered
                 
-                // remove this if in production mode
-                $oidc->setVerifyHost(false);
-                $oidc->setVerifyPeer(false);
+        //         // remove this if in production mode
+        //         $oidc->setVerifyHost(false);
+        //         $oidc->setVerifyPeer(false);
             
-                $oidc->authenticate(); //call the main function of myITS SSO login
+        //         $oidc->authenticate(); //call the main function of myITS SSO login
             
-                Session::put('id_token', $oidc->getIdToken()); // must be save for check session dan logout proccess
-                $user = $oidc->requestUserInfo(); // this will return user information from myITS SSO database
-                return view('welcome');
-            } catch (OpenIDConnectClientException $e) {
-                echo $e->getMessage();
-            }
-        } else{
-            Session::put('jenis', 3);
-            return redirect('/');
-        }
+        //         Session::put('id_token', $oidc->getIdToken()); // must be save for check session dan logout proccess
+        //         $user = $oidc->requestUserInfo(); // this will return user information from myITS SSO database
+        //         return view('welcome');
+        //     } catch (OpenIDConnectClientException $e) {
+        //         echo $e->getMessage();
+        //     }
+        // } else{
+        //     Session::put('jenis', 3);
+        //     return redirect('/');
+        // }
     }
 }
